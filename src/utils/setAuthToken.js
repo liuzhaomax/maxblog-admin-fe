33f5cb5eb4dd6f979e61fa0a4fe69fd64fec5037
LIUZHAO <liuzhaomax@163.com>
2024-03-27 22:15:44 +0800

    token

--- src/utils/setAuthToken.js
+++ src/utils/setAuthToken.js
@@ -3,7 +3,7 @@ import axios from "axios"
 const setAuthToken = token => {
     if (token) {
         axios.defaults.withCredentials = true
-        axios.defaults.headers.common["Authorization"] += token
+        axios.defaults.headers.common["Authorization"] = "Bearer " + token
     } else {
         axios.defaults.withCredentials = false
         delete axios.defaults.headers.common["Authorization"]
