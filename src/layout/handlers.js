61f3735f2a3ad9fa18bf3bb2fab728140e408ad6
LIUZHAO <liuzhaomax@163.com>
2024-03-28 00:01:29 +0800

    logout

--- src/layout/handlers.js
+++ src/layout/handlers.js
@@ -2,5 +2,5 @@ import axios from "axios"
 import { URL } from "../config/url"
 
 export const deleteLogin = () => {
-    return axios.delete(URL.INNER.Logout, {withCredentials: true})
+    return axios.delete(URL.INNER.Logout)
 }
